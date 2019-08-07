f17a924b687965faaa4c18a187691283985dd61f
Fletcher91 <thom@argu.co>
2019-08-07 20:49:09 +0200

    Fix crash when subject isn’t available

--- src/outlines/HTMLResource/HTMLResource.tsx
+++ src/outlines/HTMLResource/HTMLResource.tsx
@@ -1,4 +1,5 @@
 import { Icon, Link } from "@material-ui/core";
+import { useLinkRenderContext } from "link-redux";
 import { NamedNode } from "rdflib";
 import * as React from "react";
 
@@ -8,16 +9,20 @@ import {
   IconTopology,
 } from "../../topologies";
 
-export const HTMLResource = ({ subject }) => (
-  <Link
-   href={subject.value}
-   rel="noopener noreferrer"
-   target="_blank"
-  >
-    <Icon>open_in_new</Icon>
-    {subject.value}
-  </Link>
-);
+export const HTMLResource = ({ subject }) => {
+  const { subject: subjectCtx } = useLinkRenderContext();
+
+  return (
+    <Link
+     href={(subject || subjectCtx).value}
+     rel="noopener noreferrer"
+     target="_blank"
+    >
+      <Icon>open_in_new</Icon>
+      {(subject || subjectCtx).value}
+    </Link>
+  );
+};
 
 HTMLResource.type = [
   new NamedNode("http://www.w3.org/ns/iana/media-types/text/html#Resource"),
